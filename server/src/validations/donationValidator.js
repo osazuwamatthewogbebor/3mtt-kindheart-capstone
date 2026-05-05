865f725b4f6ecfc909dfac3f80ac132b5163b37d
Osazuwa Matthew Ogbebor <osazuwamatthewogbebor@gmail.com>
2026-05-05 10:03:21 +0100

    updated donationValidator schema

--- server/src/validations/donationValidator.js
+++ server/src/validations/donationValidator.js
@@ -1,7 +1,7 @@
 import {z} from "zod";
 
 export const donationSchema = z.object({
-    campaignId: z.uuid.({ message: "Invalid Campaign ID format" }),
+    campaignId: z.string().uuid({ message: "Invalid Campaign ID format" }),
     amount: z.coerce.number()
         .positive({ message: "Donation must be greater than 0" })
         .min(100, { message: "Minimum donation is ₦100" })
